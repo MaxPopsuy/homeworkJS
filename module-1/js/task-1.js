1f436c55c44f8c5d1689d384c5f7722ce17402e3
MaxPopsuy <maim09p@gmail.com>
2021-01-24 00:25:53 +0200

    task-1/2 added

---
+++ module-1/js/task-1.js
@@ -0,0 +1,25 @@
+// 1.
+
+let str = "Привіт";
+let num = 123;
+let flag = true;
+let txt = "true";
+
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof flag);
+console.log(typeof txt);
+
+// 2.
+
+let userName = "Ivan";
+let surname = "Melnik";
+let balance = 1500;
+let isShopOpened = false;
+let age = 30;
+
+const yearOfBirth = 2009;
+const price = 1500;
+const isWinner = true;
+const hello = "Hello world!";
+const widthCanvas = 500;
