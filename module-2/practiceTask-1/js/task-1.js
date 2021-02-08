e38766c2de5b9e2195a86b63fbd85a23d8c0075c
MaxPopsuy <maim09p@gmail.com>
2021-02-08 20:03:36 +0200

    task-1 fixed

--- module-2/practiceTask-1/js/task-1.js
+++ module-2/practiceTask-1/js/task-1.js
@@ -1,6 +1,6 @@
 let Arr1 = [1, 5, "4", "hello0"];
 let Arr2 = [true, 2, {}, ["a"], 222];
 
-console.log(Arr1[2]);
-console.log(Arr2[2]);
-console.log(Arr1[2] + Arr2[2]);
+console.log(Arr1[1]);
+console.log(Arr2[1]);
+console.log(Arr1[1] + Arr2[1]);
